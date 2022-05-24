939dd2403228a5eff6556550ac6e7880faabbe8e
Andrés Ibarra <andresm.ibarrag@unilibrebog.edu.co>
2022-05-24 17:40:14 -0500

    Route to images fixed on .html, will use public from .js file

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
+const path = require('path');
 const express = require('express');
 const app = express();
-const path = require('path');
+
+app.listen(1010, () => console.log('Server started at http:/localhost:1010 ...'));
+
 const publicFolder = path.resolve(__dirname, '../public');
 
 app.use(express.static(publicFolder));
 
-app.listen(1010, () => console.log('Server started at http:/localhost:1010 ...'));
-
-//app.get('/', (req, res) => )
+app.get('/', (req, res) => 
+    res.sendFile(path.resolve(__dirname, 'views/home.html'))
+);
